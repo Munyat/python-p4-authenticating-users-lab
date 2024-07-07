8e6e546d5781fee809e88b27b9d225e9675ce20e
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2024-07-07 23:05:16 +0300

    complete authenticating users lab

--- server/app.py
+++ server/app.py
@@ -26,6 +26,34 @@ class ClearSession(Resource):
         session['user_id'] = None
 
         return {}, 204
+    
+
+class Login(Resource):
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json()['username']
+        ).first()
+
+        session['user_id'] = user.id
+        return user.to_dict(),200
+
+class Logout(Resource):
+
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            if user:
+                return user.to_dict(), 200
+        
+        return {}, 401
+
 
 class IndexArticle(Resource):
     
@@ -49,8 +77,12 @@ class ShowArticle(Resource):
         return {'message': 'Maximum pageview limit reached'}, 401
 
 api.add_resource(ClearSession, '/clear')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+
 
 
 if __name__ == '__main__':
